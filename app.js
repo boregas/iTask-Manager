e9df1efb7c30a470b5fab7c83e4573f2537b7561
Bernardo Boregas Paulinelli <bernardobpaulinelli@gmail.com>
2026-07-05 10:43:44 -0300

    feat: projeto concluido

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 class Tarefa {
     titulo;
     descricao;
     data;
+    concluido;
     constructor(titulo, descricao) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.data = new Date();
+        this.concluido = false;
     }
-    apresentar() {
-        return `Tarefa: ${this.titulo}.\n${this.descricao}.\nData: ${this.data.toLocaleDateString()}`;
+    renderizar() {
+        const li = document.createElement('li');
+        const dataFormatada = this.data.toLocaleDateString('pt-BR');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = this.concluido;
+        checkbox.addEventListener('change', () => {
+            this.concluido = checkbox.checked;
+            console.log(this.concluido);
+            li.classList.toggle('concluida');
+        });
+        li.innerHTML = `
+            <strong>${this.titulo}</strong>
+
+            ${this.descricao ? `<p>${this.descricao}</p> ` : ''}
+
+            <small>Criado em: ${dataFormatada}</small>
+
+            `;
+        li.appendChild(checkbox);
+        return li;
     }
-    
 }
-const minhaTarefa = new Tarefa("Estudar DOM", "Começar os estudos de DOM pelo trainee da iJunior");
-console.log(minhaTarefa.apresentar());
+class App {
+    tarefas = [];
+    adicionarTarefa(novaTarefa) {
+        this.tarefas.push(novaTarefa);
+        const lista = novaTarefa.renderizar();
+        document.getElementById('lista')?.appendChild(lista);
+    }
+}
+const btn = document.getElementById('addBtn');
+const app = new App;
+btn.addEventListener('click', () => {
+    const inputT = document.getElementById('tituloInput');
+    const inputD = document.getElementById('descInput');
+    if (inputT.value === '') {
+        alert('O título é obrigatório!');
+        return;
+    }
+    const novaTarefa = new Tarefa(inputT.value, inputD.value);
+    app.adicionarTarefa(novaTarefa);
+    inputT.value = '';
+    inputD.value = '';
+});
+export {};
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,33 +1,65 @@
 class Tarefa{
     titulo: string;
-    descricao: string;
+    descricao: string | undefined;
     data: Date;
+    concluido: boolean;
 
-    constructor(titulo: string, descricao: string){
+    constructor(titulo: string, descricao?: string){
         this.titulo = titulo;
         this.descricao = descricao;
         this.data = new Date();
-    }
-
-    apresentar(){
-        return `Tarefa: ${this.titulo}.\n${this.descricao}.\nData: ${this.data.toLocaleDateString()}`;
+        this.concluido = false;
     }
 
     renderizar(){
+  
         const li = document.createElement('li');
+        const dataFormatada = this.data.toLocaleDateString('pt-BR');
+        const checkbox = document.createElement('input')
+
+        checkbox.type = 'checkbox';
+        checkbox.checked = this.concluido;
+        checkbox.addEventListener('change', () => {
+            this.concluido = checkbox.checked;
+            console.log(this.concluido);
+            li.classList.toggle('concluida');
+        });
+            
         li.innerHTML = `
             <strong>${this.titulo}</strong>
+
+            ${this.descricao ? `<p>${this.descricao}</p> ` : ''}
+
+            <small>Criado em: ${dataFormatada}</small>
+
             `;
+        li.appendChild(checkbox);
         return li;
+    }
+
+}
+
+class App{
+    tarefas: Tarefa[] = [];
+    adicionarTarefa(novaTarefa: Tarefa){
+        this.tarefas.push(novaTarefa) 
+        const lista = novaTarefa.renderizar()
+        document.getElementById('lista')?.appendChild(lista);
     }
 }
 
-//const minhaTarefa = new Tarefa("Estudar DOM", "Começar os estudos de DOM pelo trainee da iJunior");
-//console.log(minhaTarefa.apresentar())
-
 const btn = document.getElementById('addBtn') as HTMLButtonElement;
+const app = new App;
 btn.addEventListener('click', () => {
-    const input = document.getElementById('membroInput') as HTMLInputElement;
-    const novaTarefa = new Tarefa(input.value, "");
-    document.getElementById('lista')?.appendChild(novaTarefa.renderizar());
+    const inputT = document.getElementById('tituloInput') as HTMLInputElement;
+    const inputD = document.getElementById('descInput') as HTMLInputElement;
+    if(inputT.value === ''){
+        alert('O título é obrigatório!')
+        return;
+        
+    }
+    const novaTarefa = new Tarefa(inputT.value, inputD.value);
+    app.adicionarTarefa(novaTarefa);
+    inputT.value = '';
+    inputD.value = '';
 });
